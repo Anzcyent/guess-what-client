58e325f0b522a19835f57fcc6c6a3bfcfd75a86e
Cihan <ozcancihan714@gmail.com>
2023-06-18 16:59:42 +0300

    request url changed

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const getData = async (url, token) => {
-  const res = await axios.get(`http://localhost:5000${url}`, {
+  const res = await axios.get(`https://guess-what-server.onrender.com${url}`, {
     headers: { Authorization: `Bearer:${token}}` },
     withCredentials: true,
   });
@@ -10,7 +10,7 @@ export const getData = async (url, token) => {
 };
 
 export const postData = async (url, data, token) => {
-  const res = await axios.post(`http://localhost:5000${url}`, data, {
+  const res = await axios.post(`https://guess-what-server.onrender.com${url}`, data, {
     headers: { Authorization: `Bearer:${token}` },
     withCredentials: true,
   });
