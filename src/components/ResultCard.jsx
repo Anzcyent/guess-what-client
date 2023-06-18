e70a2b58fbad0d9a93b1505ce6d0d352a606462e
Cihan <ozcancihan714@gmail.com>
2023-06-18 17:20:12 +0300

    responsive bug solved

--- src/components/ResultCard.jsx
+++ src/components/ResultCard.jsx
@@ -7,7 +7,7 @@ const ResultCard = () => {
 
   return (
     showResultCard && (
-      <div className="fade sm:w-[300px] w-[150px] md:h-[100px] h-[50px] flex__center bg-lime text-white font-bold sm:text-4xl text-2xl absolute sm:top-[-100px] top-[300px] inset-12 rounded-t-xl">
+      <div className="fade sm:w-[300px] w-[150px] md:h-[100px] h-[50px] flex__center bg-lime text-white font-bold sm:text-4xl text-2xl absolute sm:top-[-100px] top-[-50px] inset-12 rounded-t-xl">
         {game.resultNumber}
       </div>
     )
